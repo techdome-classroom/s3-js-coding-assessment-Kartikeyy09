12d50f0960ab901b5e067f492cd38589cfecddc9
index.html <malviyakartikey@gmail.com>
2024-10-07 15:12:22 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,8 +3,36 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    // Define a map for Roman numeral values
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
     
+    let total = 0;
+    let prevValue = 0;
+
+    // Loop through each character in the Roman numeral string
+    for (let i = s.length - 1; i >= 0; i--) {
+        const currentValue = romanMap[s[i]];
+
+        // If current value is less than the previous value, subtract it; otherwise, add it
+        if (currentValue < prevValue) {
+            total -= currentValue;
+        } else {
+            total += currentValue;
+        }
+
+        // Update the previous value
+        prevValue = currentValue;
+    }
+
+    return total;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
